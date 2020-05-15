abd8190ff1f741393d988e7ac4f25abe72a916d1
Tolga Guceri <Guceri@gmail.com>
2020-05-15 15:25:38 -0400

    sample input file

---
+++ _inputs_sample.R
@@ -0,0 +1,14 @@
+#===========================================================================================
+#                              COINBASEPRO API INPUTS
+#===========================================================================================
+#API Keys
+my_api.key <<- "insert key here"
+my_secret <<-  "insert secret here"
+my_passphrase <<- "insert passphrase here"
+#===========================================================================================
+#                         PUSHBULLET TOGGLE & KEYS
+#===========================================================================================
+#turn notifications on/off
+Pushbullet <<- TRUE
+myEmail <<- 'insert email here'
+myAPIkey <<- 'insert key here'
